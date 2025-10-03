24c0e07acfcc13ea0628afe4adaed9534d248e72
niki-fuentes <nicole.pena.fuentes@iths.se>
2025-10-03 09:57:57 +0200

    fix prisma.ts after merge with main

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,5 @@
 // lib/prisma.ts
 
-<<<<<<< HEAD
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-
-// Undvik att skapa flera instanser i dev
-const prisma = global.prisma || new PrismaClient();
-=======
 import { PrismaClient } from "./generated/prisma";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
@@ -17,7 +9,6 @@ export const prisma =
   new PrismaClient({
     log: ["query", "error", "warn"],
   });
->>>>>>> origin/main
 
 if (process.env.NODE_ENV === "development") global.prisma = prisma;
 
